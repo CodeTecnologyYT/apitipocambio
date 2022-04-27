2e80aa90e2fc0664a174dc24ab46f04edf4818e5
Bryan Rosas <bryan.rosas.contractor@bbva.com>
2022-04-27 12:14:27 -0500

    creacion de tablas y diagrama de entidad relacion

---
+++ README.md
@@ -0,0 +1,11 @@
+# Prueba Tecnica
+## 1.- Problema
+El sistema debe poder realizar un tipo de cambio a un monto donde se deben utilizar el monto, moneda de origen, moneda de destino, monto con el tipo de campo y el tipo de cambio
+Debe Permitir el registro, actualización y búsqueda del tipo de cambio
+Debe tener una autenticación
+Por cada tipo de cambio realizado, se debe registrar quien hizo la solicitud (auditoría funcional)
+
+## 2.- Solucion
+
+### Diagrama de Entidad - Relacion
+![](diagramaRelalacion.png)
---
+++ scripts/2-tables.sql
@@ -0,0 +1,52 @@
+CREATE TABLE public.tdc_rol (
+    id varchar NULL,
+    descripcion varchar NULL,
+    CONSTRAINT tdc_rol_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE public.tdc_usuario (
+    id varchar NOT NULL,
+    username varchar NULL,
+    "password" varchar NULL,
+    activo bool NULL DEFAULT true,
+    id_role varchar NULL,
+    CONSTRAINT tdc_usuario_pk PRIMARY KEY (id),
+    CONSTRAINT tdc_usuario_un UNIQUE (username),
+    CONSTRAINT tdc_usuario_fk FOREIGN KEY (id_role) REFERENCES public.tdc_rol(id)
+);
+
+CREATE TABLE public.tdc_moneda (
+   id varchar NULL,
+   descripcion varchar NULL,
+   CONSTRAINT tdc_moneda_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE public.tdc_tipocambio (
+   id varchar NULL,
+   id_monedaentrada varchar NULL,
+   id_monedasalida varchar NULL,
+   valor decimal NULL DEFAULT 0,
+   periodo date NULL DEFAULT CURRENT_DATE,
+   activo boolean NULL DEFAULT TRUE,
+   CONSTRAINT tdc_tipocambio_pk PRIMARY KEY (id),
+   CONSTRAINT tdc_tipocambio_fk FOREIGN KEY (id_monedaentrada) REFERENCES public.tdc_moneda(id),
+   CONSTRAINT tdc_tipocambio_fk_1 FOREIGN KEY (id_monedasalida) REFERENCES public.tdc_moneda(id)
+);
+
+CREATE TABLE public.tdc_tipocambio_audit (
+     id varchar NOT NULL,
+     id_monedaentrada varchar NULL,
+     id_monedasalida varchar NULL,
+     valor numeric NULL DEFAULT 0,
+     periodo date NULL,
+     activo bool NULL DEFAULT true,
+     usuario_operacion varchar NULL,
+     fecha_operacion timestamp NULL DEFAULT CURRENT_DATE,
+     CONSTRAINT tdc_tipocambio_audit_pk PRIMARY KEY (id)
+);
+
+
+
+
+
+
